c72aac2c1bd104ec655d156ebac7c6b5d6a76cfb
Joe Streifel <josephstreifel@gmail.com>
2022-02-01 11:43:38 -0800

    works on card styling
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>

--- pages/home.js
+++ pages/home.js
@@ -2,10 +2,52 @@ import Card from "../templates/card";
 import Header from "../templates/header";
 
 export default function Test() {
+  const pikachu = {
+    tier: 1,
+    name: "Pikachu",
+    types: ["electric"],
+    sprites: {
+      frontDefault:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+      backDefault:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png"
+    },
+    art: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    stats: {
+      hp: 50,
+      attack: 40,
+      defense: 30,
+      spatk: 35,
+      spdef: 25
+    },
+    moves: [
+      {
+        name: "mega-punch",
+        power: 80,
+        accuracy: 85
+      },
+      {
+        name: "thunder",
+        power: 100,
+        accuracy: 90
+      },
+      {
+        name: "tackle",
+        power: 40,
+        accuracy: 100
+      },
+      {
+        name: "gnaw",
+        power: 30,
+        accuracy: 100
+      }
+    ]
+  };
+
   return (
     <>
       <Header />
-      <Card />
+      <Card pokemon={pikachu}/>
     </>
   );
 }
--- templates/card.js
+++ templates/card.js
@@ -1,32 +1,32 @@
 export default function Card({ pokemon }) {
-  let colors = {
-    bug: {
-      background1: "bg-green-100"
-    }
-  };
+
+  let background = pokemon.types[0]
 
   return (
     // outter border of card
     // card container
-    <div className="absolute h-80 w-60 rounded-md border border-sky-500">
+    <div className="relative h-120 w-60 rounded-md border-2 border-sky-500">
       {/* //colors["bug"].background */}
-      <div className="bg-yellow-200 border-dashed p-4 rounded-md">
-        <div className="bg-yellow-200 rounded-full bg-contain bg-center ">
-          Electric
+      <div className={background}>
+        <div className="flex items-center justify-center">
+          {pokemon.name}
         </div>
         <img
-          src="Pikachu.jpg"
+          src={pokemon.art}
           alt="Pikachu"
           width="200"
           height="400"
-          position="justify: center"
+          position="justify:center"
         />
-        <div className="sprite img "></div>
-        <div className="rarity  absolute inset-x-0">1st tier</div>
-        <div className="hp value absolute bottom- right-0">100hp</div>
-        <div className="defense stat">150 defense</div>
-        <div className="attack stat">30 attack</div>
-        <div className="abilities">Shock, Flop, Dash, Burrow</div>
+        <div className="sprite name flex items-center justify-center">Electric</div>
+        <div className="rarity  absolute top-0 right-0">1st tier</div>
+        <div className="absolute top-0 left-0">100hp</div>
+        <div className="attack and defense flex justify-around">
+          <p>{pokemon.stats.defense} defense</p>
+          <p>{pokemon.stats.attack} attack</p>
+        </div>
+        {/* <div className="attack stat">30 attack</div> */}
+        {/* <div className="abilities">Shock, Flop, Dash, Burrow</div> */}
       </div>
     </div>
   );
