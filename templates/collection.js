65eee0ff8e4c5a03391576a404505151252eb911
Joe Streifel <josephstreifel@gmail.com>
2022-02-01 11:45:02 -0800

    begins collection code
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>

---
+++ templates/collection.js
@@ -0,0 +1,11 @@
+import Card from "./card";
+
+export default function Collection(props) {
+    return(
+        <div>
+            {props.user.pokemon_collection.map((pokemon) => {
+                return
+            })}
+        </div>
+    )
+}
