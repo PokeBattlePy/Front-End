ceacd7069f216e1c262c181aeaec74b2d63bca03
Joe Streifel <josephstreifel@gmail.com>
2022-02-01 16:31:13 -0800

    allows for simulated login
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>
    Co-Authored-By: Kirk Garrison <89801438+KirkGarrison@users.noreply.github.com>

--- templates/header.js
+++ templates/header.js
@@ -1,25 +1,24 @@
 import Link from "next/link";
+import { useUser } from "../context/userContext";
 
 export default function Header() {
-  let user = false;
-  let loginSection;
-  if (user) {
-    loginSection = <LoggedIn />;
-  } else {
-    loginSection = <NotLoggedIn />;
-  }
+  const { user, login, logout } = useUser();
 
   return (
     <header className="flex justify-between p-5 border-b-4 border-black items-center">
       <div>
         <h1>PokeBattlePy</h1>
       </div>
-      {loginSection}
+      {user ? <LoggedIn logout={logout} /> : <NotLoggedIn login={login} />}
     </header>
   );
 }
 
-export function NotLoggedIn() {
+export function NotLoggedIn({ login }) {
+
+  function handleLogin() {
+    login();
+  }
   return (
     <div className="flex justify-around items-center">
       <Link href="/">
@@ -31,18 +30,29 @@ export function NotLoggedIn() {
         <label className="px-1">Password</label>
         <input className="px-1" name="password" type="password" />
       </form>
-      <button className="px-4 py-1 border-2 border-black">Login</button>
+      <button onClick={handleLogin} className="px-4 py-1 border-2 border-black">Login</button>
     </div>
   );
 }
 
-export function LoggedIn() {
+export function LoggedIn({ logout }) {
+
+  function handleLogout() {
+    logout();
+  }
+
   return (
     <div>
-      <a>Home</a>
-      <a>Deck</a>
-      <a>Battle</a>
-      <button>Logout</button>
+      <Link href="/">
+        <a className="px-2">Home</a>
+      </Link>
+      <Link href="/deck">
+        <a className="px-2">Deck</a>
+      </Link>
+      <Link href="/arena">
+        <a className="px-2">Battle</a>
+      </Link>
+      <button className="px-2" onClick={handleLogout}>Logout</button>
     </div>
   );
 }
