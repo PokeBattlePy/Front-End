f61c0abe572ffa8c498bc86ddc75e058ec978c0b
Joe Streifel <josephstreifel@gmail.com>
2022-02-03 14:02:37 -0800

    sets up basic deck modification
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>
    Co-Authored-By: Kirk Garrison <89801438+KirkGarrison@users.noreply.github.com>

--- pages/deck.js
+++ pages/deck.js
@@ -4,23 +4,43 @@ import { useTrainer } from "../hooks/useTrainer";
 import { useUser } from "../context/userContext";
 import Collection from "../templates/collection";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Deck() {
   const { tokens } = useUser();
   const { trainer } = useTrainer();
 
-  const [deck, setDeck] = useState(trainer ? trainer.decks : [])
+  return (
+    <div className="gb-text">
+      <Header />
+
+      <CardEditor deck={trainer ? trainer.decks : []} collection={trainer ? trainer.cards : []} />
+
+    </div>
+  );
+}
+
+function CardEditor(props) {
+  //THIS CAN BE USED TO DECOUPLE STATE FROM USETRAINER HOOK
+  //AAAAAHHHHHHHHH
+  //-JOE
+
+  const {tokens, user} = useUser();
+
+  const initialDeck = props.deck.map((card) => card)
+
+  const [deck, setDeck] = useState(initialDeck)
 
   const placeholder = {
     "pokemon_int": 22,
     "name": "empty",
     "rarity": "common",
-    "types": ["normal"],
+    "types": [""],
     "moves": {
       "base": { "name": "whirlwind", "power": null, "class": "status", "type": "normal" },
       "special": { "name": "razor-wind", "power": 80, "class": "special", "type": "normal" }
     },
-    "official_artwork": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/22.png",
+    "official_artwork": "/questionMark.png",
     "stats": {
       "hp": 0,
       "attack": 0,
@@ -33,75 +53,68 @@ export default function Deck() {
   function removeCard(remove_pokemon) {
     console.log("removing ", remove_pokemon.name);
 
-    let newDeck = deck.map((deck_pokemon) => deck_pokemon.name == remove_pokemon.name ? placeholder : remove_pokemon);
-
-    // while (newDeck.length < 3) {
-    //   newDeck.append(placeholder)
-    // }
-    console.log(newDeck);
+    let newDeck = deck.map((deck_pokemon) => deck_pokemon.name == remove_pokemon.name ? placeholder : deck_pokemon);
 
     setDeck(newDeck);
   }
 
   function addCard(add_pokemon) {
+    let dupe = false
+    let added = false
     deck.forEach((pokemon) => {
-      let dupe = false
-
-      if (pokemon.name == add_pokemon.name){
+      if (pokemon.name == add_pokemon.name) {
         alert(`Can't have more than 1 ${pokemon.name} in deck!`)
         dupe = true
         return
       }
-      if(dupe){
-        return
-      }
     })
-    if (deck.includes(placeholder)) {
-      newDeck = deck.map((pokemon) => pokemon);
-      for (let i = 0; i < newDeck.length; i++) {
-        if (newDeck[i] == placeholder) {
-          newDeck[i] = add_pokemon
-          break
-        }
+    if (dupe) {
+      return
+    }
+
+    let newDeck = deck.map((pokemon) => pokemon);
+    for (let i = 0; i < newDeck.length; i++) {
+      if (newDeck[i].name == placeholder.name) {
+        newDeck[i] = add_pokemon;
+        added = true;
+        setDeck(newDeck);
+        break
       }
-      setDeck(newDeck)
-    } else {
+    }
+
+    if (!added) {
       alert("Deck full! Please remove a pokemon before adding a new pokemon")
     }
   }
 
-  function handleSaveDeck() {
+  async function handleSaveDeck() {
     const info = {
       headers: {
         "Authorization": tokens.access
       },
-      body: {
+      data: {
         "decks": deck
       }
     }
     //make request to API so save deck
+    await axios.put(`https://poke-battle-py.herokuapp.com/trainer/${user.id}/`, info)
   }
 
   return (
-    <div className="gb-text">
-      <Header />
+    <div className="flex-column w-4/5 m-auto">
 
-      <div className="flex-column w-4/5 m-auto">
+      <div>
+        <h2 className="my-5">Current Deck</h2>
+        <BattleDeck deck={deck} numbered={true} removeCard={removeCard} button={true} />
+        <button className="underline" onClick={handleSaveDeck}>Save Deck</button>
+      </div>
 
-        <div>
-          <h2 className="my-5">Current Deck</h2>
-          <BattleDeck deck={trainer ? trainer.decks : []} numbered={true} removeCard={removeCard} button={true} />
-          <button className="underline" onClick={handleSaveDeck}>Save Deck</button>
-        </div>
+      <div>
+        <h2 className="my-5">Collection</h2>
 
-        <div>
-          <h2 className="my-5">Collection</h2>
-
-          <Collection collection={trainer ? trainer.cards : []} addCard={addCard} />
-        </div>
-
+        <Collection collection={props.collection} addCard={addCard} />
       </div>
 
     </div>
-  );
+  )
 }
