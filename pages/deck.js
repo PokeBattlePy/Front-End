13a1860e28c19eba48e11229c9cb9a2537cc6184
Joe Streifel <josephstreifel@gmail.com>
2022-02-02 17:30:37 -0800

    updates for use with backend info
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>
    Co-Authored-By: Kirk Garrison <89801438+KirkGarrison@users.noreply.github.com>

--- pages/deck.js
+++ pages/deck.js
@@ -1,27 +1,39 @@
 import Header from "../templates/header";
 import BattleDeck from "../templates/battleDeck";
-import { useUser } from "../context/userContext";
+import { useTrainer } from "../hooks/useTrainer";
 import Collection from "../templates/collection";
+import { useState } from "react";
 
 export default function Deck() {
-  const {trainer} = useUser();
-  console.log(trainer.pokemon_collection)
+  const {trainer} = useTrainer();
+  
+  const [deck, setDeck] = useState(trainer ? trainer.decks : [])
+
+  function removeCard(id){
+    newDeck = deck.map((pokemon) => pokemon.id == id ? placeholder : pokemon)
+
+    while (newDeck.length<3){
+      newDeck.append(placeholder)
+    }
+
+    setDeck(newDeck)
+  }
 
   return (
-    <div>
+    <div className="gb-text">
       <Header />
       
       <div className="flex-column w-4/5 m-auto">
 
       <div>
-        <h2>Current Deck</h2>
-        <BattleDeck deck={trainer.pokemon_deck.pokemon} numbered={true} />
+        <h2 className="my-5">Current Deck</h2>
+        <BattleDeck deck={trainer ? trainer.decks : []} numbered={true} removeCard={removeCard} button={true}/>
       </div>
 
       <div>
-        <h2>Collection</h2>
+        <h2 className="my-5">Collection</h2>
         
-        <Collection collection={trainer.pokemon_collection}/>
+        <Collection collection={trainer ? trainer.cards : []}/>
       </div>
 
       </div>
